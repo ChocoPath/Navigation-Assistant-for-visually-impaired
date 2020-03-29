3d54b5e43ea45e2f0dd64637d5cf58e050d57078
ChocoPath <quayeworsley@gmail.com>
2020-03-29 21:56:04 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,13 +1,17 @@
+
+
 import speech_recognition as sr
 import pyttsx3
 import os
 import time
 # import the necessary packages
 from imutils.video import FPS
+from imutils.video import VideoStream
 import numpy as np
 import imutils
 import dlib
 import cv2
+import math
 
 
 
@@ -18,11 +22,12 @@ def objectdetection(obj):
 	labels=obj
 
 	#confidence level of model
-	confidence=0.7
+	conf=0.7
 
 	#video input either stream or video file
-	#video = 0
-	video= 'input/cat.mp4'
+	video = 0
+	#video= 'input/cat.mp4'
+
 
 
 	# initialize the list of class labels MobileNet SSD was trained to
@@ -31,6 +36,10 @@ def objectdetection(obj):
 		"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 		"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 		"sofa", "train", "tvmonitor"]
+ 
+	
+	COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
 
 	# load model from disk
 	print("[INFO] loading model...")
@@ -39,7 +48,7 @@ def objectdetection(obj):
 	# initialize the video stream, dlib correlation tracker, output video
 	# writer, and predicted class label
 	print("[INFO] starting video stream...")
-	vs = cv2.VideoCapture(video)
+	vs = VideoStream(src=0).start()
 	tracker = None
 	writer = None
 	label = ""
@@ -47,131 +56,73 @@ def objectdetection(obj):
 	# start the frames per second throughput estimator
 	fps = FPS().start()
 
-	# loop over frames from the video file stream
+	
+	frame_width = 600
+	user_x = frame_width/2
+	user_y = frame_width
 	while True:
 		# grab the next frame from the video file
-		(grabbed, frame) = vs.read()
-
-		# check to see if we have reached the end of the video file
-		if frame is None:
-			break
+		frame = vs.read()
 
 		# resize the frame for faster processing and then convert the
-		# frame from BGR to RGB ordering (dlib needs RGB ordering)
-		frame = imutils.resize(frame, width=600)
-		rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-
-		# if our correlation object tracker is None we first need to
-		# apply an object detector to seed the tracker with something
-		# to actually track
-		if tracker is None:
-			# grab the frame dimensions and convert the frame to a blob
-			(h, w) = frame.shape[:2]
-			blob = cv2.dnn.blobFromImage(frame, 0.007843, (w, h), 127.5)
-
-			# pass the blob through the network and obtain the detections
-			# and predictions
-			net.setInput(blob)
-			detections = net.forward()
-
-			# ensure at least one detection is made
-			if len(detections) > 0:
-				# find the index of the detection with the largest
-				# probability -- out of convenience we are only going
-				# to track the first object we find with the largest
-				# probability; future examples will demonstrate how to
-				# detect and extract *specific* objects
-				i = np.argmax(detections[0, 0, :, 2])
-
-				# grab the probability associated with the object along
-				# with its class label
-				conf = detections[0, 0, i, 2]
-				label = CLASSES[int(detections[0, 0, i, 1])]
-
-				# filter out weak detections by requiring a minimum
-				# confidence
-				if conf > confidence and label == labels:
-					# compute the (x, y)-coordinates of the bounding box
-					# for the object
+		frame = imutils.resize(frame,width=frame_width)
+		(h, w) = frame.shape[:2]
+		blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+			0.007843, (300, 300), 127.5)
+  
+		# pass the blob through the network and obtain the detections
+		# and predictions
+		net.setInput(blob)
+		detections = net.forward()
+		fl=False
+		for i in np.arange(0, detections.shape[2]):
+			confidence = detections[0, 0, i, 2]
+			if confidence >conf:
+				idx = int(detections[0, 0, i, 1])
+				if obj in CLASSES[idx] :
+					fl=True
 					box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 					(startX, startY, endX, endY) = box.astype("int")
 
-					# construct a dlib rectangle object from the bounding
-					# box coordinates and then start the dlib correlation
-					# tracker
-					tracker = dlib.correlation_tracker()
-					rect = dlib.rectangle(startX, startY, endX, endY)
-					tracker.start_track(rgb, rect)
-
-					# draw the bounding box and text for the object
+					label = "{}: {:.2f}%".format(CLASSES[idx],
+						confidence * 100)
 					cv2.rectangle(frame, (startX, startY), (endX, endY),
-						(0, 255, 0), 2)
-					cv2.putText(frame, label, (startX, startY - 15),
-						cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
-
-		# otherwise, we've already performed detection so let's track
-		# the object
-		else:
-			# update the tracker and grab the position of the tracked
-			# object
-			tracker.update(rgb)
-			pos = tracker.get_position()
-
-			# unpack the position object
-			startX = int(pos.left())
-			startY = int(pos.top())
-			endX = int(pos.right())
-			endY = int(pos.bottom())
-
-			# draw the bounding box from the correlation object tracker
-			cv2.rectangle(frame, (startX, startY), (endX, endY),
-				(0, 255, 0), 2)
-			cv2.putText(frame, label, (startX, startY - 15),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
-   
-			cv2.line(frame,(startX,endX),(startY,endY),(255,0,0),)
-				
-		# check to see if we should write the frame to disk
-		if writer is not None:
-			writer.write(frame)
-
-		# show the output frame
+						COLORS[idx], 2)
+					y = startY - 15 if startY - 15 > 15 else startY + 15
+					cv2.putText(frame, label, (startX, y),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+					centerX = (endX+startX)/2
+					centerY = (endY+startY)/2
+					cv2.line(frame,(int(user_x),int(user_y)),(int(centerX),int(centerY)),(255,0,0),7)
+					dir =0
+					if centerX > user_x :
+						dir =1
+					elif centerX < user_x :
+						dir = -1
+					deg = math.degrees(math.atan(abs(centerX-user_x)/abs(centerY-user_y)))
+					
 		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
-
-		# if the `q` key was pressed, break from the loop
+		key = cv2.waitKey(1)  & 0xFF
 		if key == ord("q"):
-			break
-
-		# update the FPS counter
+			break		
 		fps.update()
 
-	# stop the timer and display FPS information
 	fps.stop()
 	print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
 	print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-	# check to see if we need to release the video writer pointer
-	if writer is not None:
-		writer.release()
-
-	# do a bit of cleanup
 	cv2.destroyAllWindows()
-	vs.release()
-
+	vs.stop()
+	return fl,deg,dir
 
 duration = 0.5
 freq = 440
 engine = pyttsx3.init()
 engine.setProperty('rate', 150)
 #engine.setProperty('voice', 'english+f1')
-def beep() :
-	os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (duration, freq))
-	
 
+	
 # Speech to Text conversion using google API
-engine.say("Welcome to the Navigation assistant. Speak help me to activate")
+engine.say("Welcome to the Navigation assistant. Say help me to activate")
 engine.runAndWait()
 r = sr.Recognizer()
 with sr.Microphone() as source:
@@ -238,5 +189,4 @@ with sr.Microphone() as source:
 			except:
 				time.sleep(3)
 				count +=1
-
 
